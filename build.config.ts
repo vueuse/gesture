1caa7d0cb9e58d701099cd6c172fc9b7bdca6b74
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-01-01 00:26:58 +0100

    chore(package): config unbuild and watch script

---
+++ build.config.ts
@@ -0,0 +1,15 @@
+import { defineBuildConfig } from 'unbuild'
+
+export default defineBuildConfig({
+  declaration: true,
+  entries: [{ input: 'src/index.ts', outDir: 'dist', name: 'index' }],
+  externals: [
+    'csstype',
+    '@vueuse/shared',
+    'framesync',
+    'style-value-types',
+    '@vue/compiler-core',
+    '@babel/parser',
+    '@vue/shared',
+  ],
+})
