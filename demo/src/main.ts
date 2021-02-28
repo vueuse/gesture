0d26a49eb4f76bdbd98319df35e6c36f4812e91e
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-02-28 23:23:32 +0100

    :sparkles: (demo) add @vueuse/motion plugin to demo

--- demo/src/main.ts
+++ demo/src/main.ts
@@ -1,6 +1,11 @@
+import { MotionPlugin } from '@vueuse/motion'
 import { createApp } from 'vue'
 import 'windi.css'
 import App from './App.vue'
 import './index.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(MotionPlugin)
+
+app.mount('#app')
