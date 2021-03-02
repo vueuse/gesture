d16e8a20710e3e0ea152d26f3d26845aa28dc70e
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-03-02 22:46:47 +0100

    :sparkles: (recognizers) import react-use-gesture recognizers

---
+++ src/recognizers/ScrollRecognizer.ts
@@ -0,0 +1,58 @@
+import { addBindings } from '../Controller'
+import { getGenericEventData, getScrollEventValues } from '../utils/event'
+import { calculateAllGeometry } from '../utils/math'
+import CoordinatesRecognizer from './CoordinatesRecognizer'
+import { getGenericPayload, getStartGestureState } from './Recognizer'
+
+export class ScrollRecognizer extends CoordinatesRecognizer<'scroll'> {
+  readonly ingKey = 'scrolling'
+  readonly stateKey = 'scroll'
+  debounced = true
+
+  handleEvent = (event: UIEvent): void => {
+    if (!this.enabled) return
+
+    this.clearTimeout()
+    this.setTimeout(this.onEnd)
+
+    const values = getScrollEventValues(event, this.transform)
+    this.updateSharedState(getGenericEventData(event))
+
+    if (!this.state._active) {
+      this.updateGestureState({
+        ...getStartGestureState(this, values, event),
+        ...getGenericPayload(this, event, true),
+        initial: this.state.values,
+      })
+
+      const movementDetection = this.getMovement(values)
+      const geometry = calculateAllGeometry(movementDetection.delta!)
+
+      this.updateGestureState(movementDetection)
+      this.updateGestureState(geometry)
+    } else {
+      this.updateGestureState({
+        ...getGenericPayload(this, event),
+        ...this.getKinematics(values, event),
+      })
+    }
+
+    this.fireGestureHandler()
+  }
+
+  onEnd = (): void => {
+    this.clean()
+    if (!this.state._active) return
+    this.updateGestureState({
+      ...this.getMovement(this.state.values),
+      _active: false,
+      velocities: [0, 0],
+      velocity: 0,
+    })
+    this.fireGestureHandler()
+  }
+
+  addBindings(bindings: any): void {
+    addBindings(bindings, 'onScroll', this.handleEvent)
+  }
+}
---
+++ src/recognizers/WheelRecognizer.ts
@@ -0,0 +1,58 @@
+import { addBindings } from '../Controller'
+import { getGenericEventData, getWheelEventValues } from '../utils/event'
+import { addV, calculateAllGeometry } from '../utils/math'
+import CoordinatesRecognizer from './CoordinatesRecognizer'
+import { getGenericPayload, getStartGestureState } from './Recognizer'
+
+export class WheelRecognizer extends CoordinatesRecognizer<'wheel'> {
+  readonly ingKey = 'wheeling'
+  readonly stateKey = 'wheel'
+  debounced = true
+
+  handleEvent = (event: WheelEvent): void => {
+    if (event.ctrlKey && 'pinch' in this.controller.handlers) return
+    if (!this.enabled) return
+
+    this.setTimeout(this.onEnd)
+    this.updateSharedState(getGenericEventData(event))
+
+    const values = addV(
+      getWheelEventValues(event, this.transform),
+      this.state.values,
+    )
+
+    if (!this.state._active) {
+      this.updateGestureState({
+        ...getStartGestureState(this, values, event),
+        ...getGenericPayload(this, event, true),
+        initial: this.state.values,
+      })
+
+      const movement = this.getMovement(values)
+      const geometry = calculateAllGeometry(movement.delta!)
+
+      this.updateGestureState(movement)
+      this.updateGestureState(geometry)
+    } else {
+      this.updateGestureState({
+        ...getGenericPayload(this, event),
+        ...this.getKinematics(values, event),
+      })
+    }
+
+    this.fireGestureHandler()
+  }
+
+  onEnd = (): void => {
+    this.clean()
+    if (!this.state._active) return
+    const movement = this.getMovement(this.state.values)
+    this.updateGestureState(movement)
+    this.updateGestureState({ _active: false, velocities: [0, 0], velocity: 0 })
+    this.fireGestureHandler()
+  }
+
+  addBindings(bindings: any): void {
+    addBindings(bindings, 'onWheel', this.handleEvent)
+  }
+}
