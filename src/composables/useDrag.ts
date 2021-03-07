4c722f47dd96fdd99dbb4387da737dd220c9de2e
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-03-07 19:22:43 +0100

    :sparkles: (useDrag) update useDrag to work with @vueuse/motion

--- src/composables/useDrag.ts
+++ src/composables/useDrag.ts
@@ -1,8 +1,14 @@
+import {
+  isMotionInstance,
+  MotionInstance,
+  MotionVariants,
+} from '@vueuse/motion'
 import { ref } from 'vue-demi'
 import { DragRecognizer } from '../recognizers/DragRecognizer'
 import { RecognizersMap } from '../recognizers/Recognizer'
 import { EventTypes, Handler, UseDragConfig } from '../types'
 import memoize from '../utils/memoize-one'
+import { resolveFromMotionInstance } from '../utils/motion'
 import isEqual from '../utils/react-fast-compare'
 import { _buildDragConfig } from './buildConfig'
 import useRecognizers from './useRecognizers'
@@ -13,10 +19,23 @@ import useRecognizers from './useRecognizers'
  * @param handler - the function fired every time the drag gesture updates
  * @param [config={}] - the config object including generic options and drag options
  */
-export function useDrag<K = EventTypes['drag']>(
-  handler: Handler<'drag', K>,
+export function useDrag<T extends MotionVariants, K = EventTypes['drag']>(
+  handler: Handler<'drag', K> | MotionInstance<T>,
   config: UseDragConfig | {} = {},
 ) {
+  let _config: UseDragConfig
+  let _handler: Handler<'drag', K>
+
+  if (isMotionInstance(handler)) {
+    const resolved = resolveFromMotionInstance<'drag', K>(handler, config)
+
+    _config = resolved.config
+    _handler = resolved.handler
+  } else {
+    _config = config as UseDragConfig
+    _handler = handler as Handler<'drag', K>
+  }
+
   RecognizersMap.set('drag', DragRecognizer)
 
   const buildDragConfig = ref()
@@ -25,10 +44,5 @@ export function useDrag<K = EventTypes['drag']>(
     buildDragConfig.value = memoize(_buildDragConfig, isEqual)
   }
 
-  const recognizers = useRecognizers<UseDragConfig>(
-    { drag: handler },
-    buildDragConfig.value(config),
-  )
-
-  recognizers()
+  useRecognizers({ drag: _handler }, buildDragConfig.value(_config))
 }
