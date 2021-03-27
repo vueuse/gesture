e8883cf353d85c59f32c93517f9bab4838e9e2f4
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-03-27 14:43:56 +0100

    :pencil: (scroll) implement scroll example

--- docs/.vitepress/components/demos/ScrollExample.vue
+++ docs/.vitepress/components/demos/ScrollExample.vue
@@ -9,19 +9,52 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
-import { useMove, useHover, subV } from '@vueuse/gesture'
+import { ref, onMounted } from 'vue'
+import { useScroll } from '@vueuse/gesture'
 import { useMotionProperties, useSpring } from '@vueuse/motion'
+import { interpolate } from 'popmotion'
 
 const demoBox = ref()
+let boxHeight = 0
 const demoElement = ref()
 
 const { motionProperties } = useMotionProperties(demoElement, {
-  x: 0,
-  y: 0,
+  backgroundColor: '#0000FF',
 })
 
-const { set } = useSpring(motionProperties)
+const mapper = interpolate([0, 50, 100], ['#0000FF', '#00FF00', '#FF0000'])
+
+const { set } = useSpring(motionProperties, {
+  damping: 30,
+  stiffness: 320,
+})
+
+useScroll(
+  ({ xy: [x, y], ...state }) => {
+    set({
+      backgroundColor: mapper((y / boxHeight) * 100),
+    })
+  },
+  {
+    domTarget: demoBox,
+  },
+)
+
+onMounted(() => {
+  boxHeight = demoBox.value.scrollHeight - 320
+
+  console.log(boxHeight)
+})
 </script>
 
-<style lang="postcss" scoped></style>
+<style lang="postcss" scoped>
+.demo-box {
+  align-items: start;
+  overflow-y: scroll;
+}
+
+.demo-element {
+  height: 300vh;
+  border-radius: 0;
+}
+</style>
