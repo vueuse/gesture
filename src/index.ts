88ca00ec451bfdb52a0e8efbdb773a90d3f61f19
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-03-29 00:54:35 +0200

    :sparkles: (exports) ..
    ..

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,15 @@ export { useMove } from './composables/useMove'
 export { usePinch } from './composables/usePinch'
 export { useScroll } from './composables/useScroll'
 export { useWheel } from './composables/useWheel'
+export {
+  drag as dragDirective,
+  hover as hoverDirective,
+  move as moveDirective,
+  pinch as pinchDirective,
+  scroll as scrollDirective,
+  wheel as wheelDirective,
+} from './directives'
+export { GesturePlugin } from './plugin'
 export * from './types/index'
 export { addV, subV } from './utils/math'
 export { rubberbandIfOutOfBounds } from './utils/rubberband'
