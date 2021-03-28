687c78dc646d098afa78daed2cfdd84a233e8bbc
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-03-29 00:42:50 +0200

    :label: (global) update global typings

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,4 +1,26 @@
+import Controller from '../Controller'
+
 // Global compile-time constants
 declare var __DEV__: boolean
 declare var __BROWSER__: boolean
 declare var __CI__: boolean
+
+type GestureHandlersMap = {
+  drag?: Controller
+  hover?: Controller
+  move?: Controller
+  pinch?: Controller
+  scroll?: Controller
+  wheel?: Controller
+  [key: string]: Controller
+}
+
+declare global {
+  interface HTMLElement {
+    gestures?: GestureHandlersMap
+  }
+
+  interface SVGElement {
+    gestures?: GestureHandlersMap
+  }
+}
