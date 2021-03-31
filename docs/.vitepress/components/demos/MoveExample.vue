22ecb05ee7bfc9603a39c6756172eb34982a6aa5
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-04-01 00:46:36 +0200

    :pencil: (move) reset rubber ball on leave

--- docs/.vitepress/components/demos/MoveExample.vue
+++ docs/.vitepress/components/demos/MoveExample.vue
@@ -6,11 +6,12 @@
 
 <script setup>
 import { ref, onMounted } from 'vue'
-import { useMove } from '@vueuse/gesture'
+import { useMove, useHover } from '@vueuse/gesture'
 import { useMotionProperties, useSpring } from '@vueuse/motion'
 
 const demoBox = ref()
 let boxRect = {}
+let hovered = false
 const demoElement = ref()
 
 const { motionProperties } = useMotionProperties(demoElement, {
@@ -19,12 +20,14 @@ const { motionProperties } = useMotionProperties(demoElement, {
 })
 
 const { set } = useSpring(motionProperties, {
-  damping: 5,
+  damping: 8,
   stiffness: 400,
 })
 
 useMove(
-  ({ event, moving, ...state }) => {
+  ({ event, moving, hovering }) => {
+    if (!hovered) return
+
     const x = event.pageX - boxRect.left - boxRect.width / 2
     const y = event.pageY - boxRect.top - boxRect.height / 2
 
@@ -41,6 +44,18 @@ useMove(
 onMounted(() => {
   boxRect = demoBox.value.getBoundingClientRect()
 })
+
+useHover(
+  ({ hovering, event }) => {
+    hovered = hovering
+
+    if (!hovering) {
+      set({ x: 0, y: 0 })
+      return
+    }
+  },
+  { domTarget: demoBox },
+)
 </script>
 
 <style lang="postcss" scoped>
