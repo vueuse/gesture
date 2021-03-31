e46faa2dc3928e660f377c392d1ed09fd0a74416
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-04-01 00:46:49 +0200

    :pencil: (pinch) implement pinch example :)

--- docs/.vitepress/components/demos/PinchExample.vue
+++ docs/.vitepress/components/demos/PinchExample.vue
@@ -5,10 +5,12 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
-import { useMove, useHover, subV } from '@vueuse/gesture'
+import { ref, onMounted, onUnmounted, watch, reactive, inject } from 'vue'
+import { usePinch, useHover } from '@vueuse/gesture'
 import { useMotionProperties, useSpring } from '@vueuse/motion'
+import { interpolate } from 'popmotion'
 
+const interacting = inject('interacting')
 const demoBox = ref()
 const demoElement = ref()
 
@@ -17,7 +19,59 @@ const { motionProperties } = useMotionProperties(demoElement, {
   y: 0,
 })
 
-const { set } = useSpring(motionProperties)
-</script>
+const springValues = reactive({ zoom: 0, rotateZ: 0 })
 
-<style lang="postcss" scoped></style>
+const { set, values } = useSpring(springValues)
+
+const mapper = interpolate([-2000, 0, 2000], [0.25, 1, 4], { clamp: true })
+
+// Cast values
+watch(
+  springValues,
+  (newVal) => {
+    Object.assign(motionProperties, {
+      rotateZ: newVal.rotateZ,
+      scale: mapper(newVal.zoom),
+    })
+  },
+  {
+    deep: true,
+  },
+)
+
+usePinch(
+  ({ offset: [d, a], pinching }) => {
+    set({ zoom: d, rotateZ: a })
+  },
+  {
+    domTarget: demoBox,
+    eventOptions: {
+      passive: true,
+    },
+  },
+)
+
+// Disable viewport pinch zoom on whole app
+
+const cancelEvent = (e) => e.preventDefault()
+
+useHover(
+  ({ hovering }) => {
+    if (!hovering) {
+      window.removeEventListener('wheel', cancelEvent, { passive: false })
+      document.removeEventListener('gesturestart', cancelEvent)
+      document.removeEventListener('gesturechange', cancelEvent)
+      interacting.value = false
+      return
+    }
+
+    window.addEventListener('wheel', cancelEvent, { passive: false })
+    document.addEventListener('gesturestart', cancelEvent)
+    document.addEventListener('gesturechange', cancelEvent)
+    interacting.value = true
+  },
+  {
+    domTarget: demoBox,
+  },
+)
+</script>
