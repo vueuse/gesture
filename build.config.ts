0701e4b92bc0b9025c34625327903e448f448c8e
Yaël GUILLOUX <yael.guilloux@gmail.com>
2022-01-04 15:01:59 +0100

    chore(build): update build config to output .mjs and .cjs

--- build.config.ts
+++ build.config.ts
@@ -1,9 +1,28 @@
 import { defineBuildConfig } from 'unbuild'
 
 export default defineBuildConfig({
+  rollup: {
+    emitCJS: true,
+  },
   declaration: true,
-  entries: [{ input: 'src/index.ts', outDir: 'dist', name: 'index' }],
+  entries: [
+    {
+      input: 'src/index.ts',
+      outDir: 'dist',
+      name: 'index',
+      format: 'esm',
+      ext: 'mjs',
+    },
+    {
+      input: 'src/index.ts',
+      outDir: 'dist',
+      name: 'index',
+      format: 'cjs',
+      ext: 'cjs',
+    },
+  ],
   externals: [
+    'vue',
     'csstype',
     '@vueuse/shared',
     'framesync',
